58671537f990e4d1aa3534422b65d68aeb85183e
pedjaceli <yannick.kananin@outlook.com>
2026-04-18 16:42:39 -0400

    bi-bag

--- js/expenses.js
+++ js/expenses.js
@@ -593,7 +593,7 @@ function renderProductAggregation() {
   if (rows.length === 0) {
     container.innerHTML = `
       <div class="empty-state">
-        <i class="bi bi-boxes"></i>
+        <i class="bi bi-bag"></i>
         <p>${t('agg_empty')}</p>
       </div>`;
     return;
